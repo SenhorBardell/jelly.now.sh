5a7b65f0817bbc9d87ca0977bd085133a3f1c373
Vlad <senhorbardell@gmail.com>
2018-10-27 13:15:12 +0700

    Fix build

--- pages/index.test.js
+++ pages/index.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import dom from "react-dom";
-import Page from "./index";
+import Page from "../pages/index";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
