a5577ed38e3bef58382160820611c17a74261489
Vlad <senhorbardell@gmail.com>
2018-11-03 21:56:12 +0700

    update ci

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,9 +1,9 @@
-import React from "react";
-import dom from "react-dom";
-import Page from "../pages/index";
+import React from 'react'
+import dom from 'react-dom'
+import Page from '../pages/index'
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  dom.render(<Page />, div);
-  dom.unmountComponentAtNode(div);
-});
+it('renders without crashing', () => {
+  const div = document.createElement('div')
+  dom.render(<Page />, div)
+  dom.unmountComponentAtNode(div)
+})
